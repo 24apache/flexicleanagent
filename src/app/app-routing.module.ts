5c9850a24d181a1ee363a580a1fd82646a570600
Pradeepgovinda <pgovindaraj710@gmail.com>
2023-06-28 19:07:05 +0530

    sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,29 @@
-import { NgModule, createComponent } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
-import { ReportsComponent } from './components/reports/reports.component';
-import { SettingsComponent } from './components/settings/settings.component';
 import { AccountComponent } from './components/account/account.component';
-import { OrdersComponent } from './components/orders/orders.component';
-// import { CreateComponent } from './components/orders/create/create.component';
-import { CustomersComponent } from './components/customers/customers.component';
-import { WalletComponent } from './components/wallet/wallet.component';
-import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+
+
+
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    children: [
-      { path: '', component: HomeComponent },
-      { path: 'account', component: AccountComponent },
-      { path: 'orders', component: OrdersComponent },
-      { path: 'customers', component: CustomersComponent },
-      { path: 'wallet', component: WalletComponent },
-      { path: 'reports', component: ReportsComponent },
-      { path: 'settings', component: SettingsComponent },
-    ]
-  },
+
+  { path: '', component: SidebarComponent },
   // { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent}
-];
+  // { path: 'account', component: AccountComponent },
+  // { path: 'orders', component: OrdersComponent },
+  // { path: 'customers', component: CustomersComponent },
+  // { path: 'wallet', component: WalletComponent },
+  // { path: 'reports', component: ReportsComponent },
+  // { path: 'settings', component: SettingsComponent },
+
+  // { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  // { path: 'forgotpassword', component: ForgotPasswordComponent},
+  // { path: 'verifyed', component: OtpVerifyedComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
