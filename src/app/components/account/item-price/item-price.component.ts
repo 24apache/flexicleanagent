6f7e443009ff2df0b7ab492c2bd425194afec10d
Vinod Yadav <24apache@gmail.com>
2023-07-11 09:27:34 +0530

    Tabs added on account page.

---
+++ src/app/components/account/item-price/item-price.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-price',
+  templateUrl: './item-price.component.html',
+  styleUrls: ['./item-price.component.scss']
+})
+export class ItemPriceComponent {
+
+}
