5d5987d082a4b1a49dae04b07119268c85709d3e
Pradeepgovinda <pgovindaraj710@gmail.com>
2023-07-22 22:52:30 +0530

    create-order

--- src/app/components/create-order/create-order.component.ts
+++ src/app/components/create-order/create-order.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-create-order',
   templateUrl: './create-order.component.html',
+  providers: [NgbAccordionConfig],
   styleUrls: ['./create-order.component.scss']
 })
 export class CreateOrderComponent {
