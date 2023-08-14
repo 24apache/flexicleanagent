39ddfae1f195a067934ab669175b367725fc455b
Pradeepgovinda <pgovindaraj710@gmail.com>
2023-08-14 22:23:55 +0530

    partner-page

--- src/app/auth/preview-payment/preview-payment.component.ts
+++ src/app/auth/preview-payment/preview-payment.component.ts
@@ -1,35 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
 
 declare var window: any;
 
 @Component({
   selector: 'app-preview-payment',
   templateUrl: './preview-payment.component.html',
-  providers: [NgbAccordionConfig],
   styleUrls: ['./preview-payment.component.scss']
   
 })
-export class PreviewPaymentComponent implements OnInit {
+export class PreviewPaymentComponent {
 
-  exform!: FormGroup;
+  // exform!: FormGroup;
 
-  ngOnInit() {
+  // ngOnInit() {
 
-    this.exform = new FormGroup({
-      'email' : new FormControl(null, [Validators.required, Validators.email]),
-      'password': new FormControl (null, [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,16}$')])
-    })
+  //   this.exform = new FormGroup({
+  //     'email' : new FormControl(null, [Validators.required, Validators.email]),
+  //     'password': new FormControl (null, [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,16}$')])
+  //   })
 
-  }
+  // }
 
 
   //MODAL
   
   formModal: any;
 
-  ngOnInits(): void{
+  ngOnInit(): void{
     this.formModal = new window.bootstrap.Modal(
       document.getElementById("exampleModal")
     )
@@ -42,7 +40,5 @@ export class PreviewPaymentComponent implements OnInit {
   doSomething(){
     this.formModal.hide();
   }
-
-
   
 }
