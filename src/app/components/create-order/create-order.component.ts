bc821e42ea3c4a41b0401da7b80667de2b8cbff6
Pradeepgovinda <pgovindaraj710@gmail.com>
2023-09-02 19:35:21 +0530

    desgin fixes

--- src/app/components/create-order/create-order.component.ts
+++ src/app/components/create-order/create-order.component.ts
@@ -1,9 +1,5 @@
-import { Component, Input, Output, EventEmitter, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, Output, EventEmitter, VERSION } from '@angular/core';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-import { NgbAlert, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgIf } from '@angular/common';
 
 
 declare var window: any;
@@ -13,10 +9,9 @@ declare var window: any;
   templateUrl: './create-order.component.html',
   providers: [NgbAccordionConfig],
   styleUrls: ['./create-order.component.scss'],
-  // standalone: true,
-	// imports: [NgIf, NgbAlertModule],
+
 })
-export class CreateOrderComponent implements OnInit {
+export class CreateOrderComponent {
 
   //MODAL
   
@@ -62,31 +57,7 @@ export class CreateOrderComponent implements OnInit {
   }
 
 
-  //ALERT
-
-  private _success = new Subject<string>();
-
-	staticAlertClosed = false;
-	successMessage = '';
-
-	@ViewChild('staticAlert', { static: false })
-  staticAlert!: NgbAlert;
-	@ViewChild('selfClosingAlert', { static: false })
-  selfClosingAlert!: NgbAlert;
-
-	ngOnInit(): void {
-		setTimeout(() => this.staticAlert.close(), 20000);
-
-		this._success.subscribe((message) => (this.successMessage = message));
-		this._success.pipe(debounceTime(50000)).subscribe(() => {
-			if (this.selfClosingAlert) {
-				this.selfClosingAlert.close();
-			}
-		});
-	}
-
-	public changeSuccessMessage() {
-		this._success.next(`Amazing ! Added to Basket`);
-	}
+  name = 'Angular ' + VERSION.major;
+  show = false
 
 }
