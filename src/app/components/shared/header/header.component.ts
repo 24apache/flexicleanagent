2e728f89de55a5cfc9cf66deb691724c67d28c3c
Pradeepgovinda <pgovindaraj710@gmail.com>
2023-09-14 17:16:54 +0530

    fixed

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  
 })
 export class HeaderComponent {
+
 
 }
