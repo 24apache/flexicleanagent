5b83fed626aca25474a73f02f58ea54d49f1220b
Vinod Yadav <24apache@gmail.com>
2023-10-03 14:39:41 +0530

    Pagination active class logic done.

--- src/app/components/customers/customers-list/customers-list.component.ts
+++ src/app/components/customers/customers-list/customers-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.scss']
+  styleUrls: ['./customers-list.component.scss'],
 })
 export class CustomersListComponent {
+  activePage: number = 1;
 
+  nextPage() {
+    if (this.activePage < 7) {
+      this.activePage++;
+    }
+  }
+
+  previousPage() {
+    if (this.activePage > 1) {
+      this.activePage--;
+    }
+  }
 }
